13d9168052cd2684001b2d146f97395b44513067
Vera Beketova <vera_beketova@mail.ru>
2020-08-17 10:55:15 +0400

    first commit

---
+++ TravioTests/BackOffice/Script/Unit1.js
@@ -0,0 +1,18 @@
+﻿function Test2()
+{
+var fin= Aliases.Travio_BackOffice.FindChildEx("Name","*HwndSource: DockWindow*");
+var tabbar = fin.FindChildEx("Name", "*ContentControl*",2);
+var devices= tabbar.FindChild("WPFControlText","*Схема*",6);
+devices.Click();
+//WPFObject("HwndSource: DockWindow", "")
+//"HwndSource: DockWindow"
+  var configurationTabControl = Aliases.Travio_BackOffice.HwndSource_DockWindow.DockWindow.ConfigurationTabControl;
+  configurationTabControl.ScrollViewer.VScroll.Pos = 0;
+  configurationTabControl.StackPanel.Grid.Combobox.Click(52, 22);
+
+if(b==1)
+{
+
+  }
+
+}
